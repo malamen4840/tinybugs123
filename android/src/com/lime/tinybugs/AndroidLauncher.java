7c68f4a6634b0ef1acc93f314d801e6f30f5e1fa
malamen4840 <jorgelima484@gmail.com>
2024-05-17 15:26:41 +0100

    sxa

---
+++ android/src/com/lime/tinybugs/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.lime.tinybugs;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.lime.tinybugs.TinyBugsGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TinyBugsGame(), config);
+	}
+}
